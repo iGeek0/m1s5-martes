b4e6c1ad0b2728a82970bf1bcca44031a487290e
iGeek <jesus.cardenas.d@gmail.com>
2023-01-17 20:38:56 -0600

    entrando a tema de tipos de datos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
-console.log(`Hola mundo 1`);
-console.log('Hola mundo 2');
-console.log("Hola mundo 2");
+"use strict";
+
+//console.log(`Hola mundo 1`);
+//console.log('Hola mundo 2');
+//console.log("Hola mundo 2");
+
+var nombre = "Juan"; // se puede reasignar pero tiene otro alcanze mas global...no se recomienda en la actualidad su uso en proyectos nuevos, salvo que se requiera ESPECIFICAMENTE
+var edad = 30;
+
+const fechaNacimiento = "17/08/1993"; // no se puede reasignar(no deberia)
+
+
+let apellido = "Perez"; // se puede reasignar
+console.log(apellido);
+
+let nombreCompleto = "Jesus Cardenas";
+
+/*
+TODO ESTO ES UN COMENTARIO
+string
+number
+boolean
+null
+undefined
+*/
+
+
+
