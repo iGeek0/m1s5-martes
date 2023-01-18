d00633fa6342a025076e1e611e1a66857aa610f3
iGeek <jesus.cardenas.d@gmail.com>
2023-01-17 21:32:47 -0600

    corte de clase martes

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,26 @@ const fechaNacimiento = "17/08/1993"; // no se puede reasignar(no deberia)
 let apellido = "Perez"; // se puede reasignar
 console.log(apellido);
 
-let nombreCompleto = "Jesus Cardenas";
+let nombreCompleto = "Jesus Cardenas"; // string(texto)
+let telefono = 1234567890; // number(numero)
+let humano = true; // boolean(true/false)
+let direccion; // undefined
+let codigoPostal = null; // null
+/*
+console.log("nombreCompleto", typeof nombreCompleto);
+console.log("telefono", typeof telefono);
+console.log("humano", typeof humano);
+console.log("direccion", typeof direccion);
+console.log("codigoPostal", typeof codigoPostal);
+*/
+
+nombreCompleto = String(123);
+telefono = Number("98765");
+console.log("nombreCompleto", nombreCompleto);
+console.log("telefono", telefono);
+console.log("humano", humano);
+console.log("direccion", direccion);
+console.log("codigoPostal", codigoPostal);
 
 /*
 TODO ESTO ES UN COMENTARIO
---
+++ assets/js/otro.js
@@ -0,0 +1,17 @@
+console.log("Console log desde otro.js");
+
+let carnName = "Volvo";
+let driver = "Juan";
+let x = 50;
+let a = "5";
+let b = "10";
+
+let suma = Number(a) + Number(b);
+
+let concatenacion = carnName + driver;
+
+console.log("La suma es:", suma);
+
+console.log("La concatenacion es:", concatenacion);
+
+
